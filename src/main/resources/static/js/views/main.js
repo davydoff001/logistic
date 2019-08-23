a32d4ac77d428f6a1741cd0bc5ffb9e517f436d6
a.davydov <a.davydov@KV-KYI-CRP-M131.ukravto.loc>
2019-08-23 12:27:12 +0300

    Spring Boot + webix

---
+++ src/main/resources/static/js/views/main.js
@@ -0,0 +1,18 @@
+define(function(cars){
+    return {
+        type:"line",
+        height: 400,
+        rows: [           
+            {
+                view: 'button',
+                label: 'Cars',
+                click: function() {
+                    routie('cars')
+                }
+            },
+            { template:"Row 1"},
+            { template:"Row 2"}
+        ]
+    }
+})
+
